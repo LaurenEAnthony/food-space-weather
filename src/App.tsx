8ac2b8df4a5e1ff8c923ede25447d65b2f092879
thecoder-droid <thegabrielfrancis@gmail.com>
2020-09-14 18:23:54 -0400

    message

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import NASA from "./NASA/NASA";
 
 function App() {
-  return <div>FOOD SPACE WEATHER</div>;
+  // const nasaURL = "https://api.nasa.gov/planetary/earth/imagery?lon=-95.33&lat=29.78&date=2018-01-01&dim=0.15&api_key=L4chJoMiegg07Kxfh4Fprho5kBMbQJxaJtyf5Qax"
+  return (<div>FOOD SPACE WEATHER
+    <NASA/>
+  </div>
+  );
 }
 
 export default App;
