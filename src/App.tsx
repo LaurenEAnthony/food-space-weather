4d04cd6d2c265440f25fa63d6b50f6d298dbed0d
Marina Iv <marisha@Marinas-MBP.home>
2020-09-14 23:26:01 -0400

    material ui is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
-import { Container, Divider } from "@material-ui/core";
+import { Container, Divider, Theme } from "@material-ui/core";
 import React from "react";
 import "./App.css";
 import NASA from "./NASA/NASA";
-import Restaurants from './Restaurants/Restaurants'
+import Restaurants from "./Restaurants/Restaurants";
 import OpenWeather from "./openWeather/openWeather";
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from "@material-ui/core/styles";
-
-
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { withStyles, createStyles } from "@material-ui/core";
+import {
+  unstable_createMuiStrictModeTheme as createMuiTheme,
+  MuiThemeProvider,
+} from "@material-ui/core/styles";
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#604439",
+    },
+    secondary: {
+      main: "#B69FF8",
+    },
+    error: {
+      main: "#111344",
+    },
+    info: {
+      main: "#dae0f2",
+    },
+    warning: {
+      main: "#efe9e7",
+    },
+  },
+});
 // const useStyles = makeStyles({
 //   root: {
 //     flexGrow: 1,
@@ -19,32 +40,23 @@ import { withStyles } from "@material-ui/core/styles";
 //   }
 // });
 
-const styles = theme: any => ({
-  root: {
-    backgroundColor: "red"
-  }
-});
-
+export const styles = (theme: Theme) =>
+  createStyles ({
+    root: {backgroundColor: 'red'}
+  });
 export interface AppState {
   latitude: number;
   longitude: number;
 }
-
-export interface AppProps {}
-
-
-
+export interface AppProps {
+  classes: any;
+}
 class App extends React.Component<AppProps, AppState> {
-
- 
-
-
   constructor(props: any) {
     super(props);
     this.state = { latitude: 0, longitude: 0 };
     console.log(this.state);
   }
-
   componentDidMount() {
     function getLocation() {
       if (navigator.geolocation) {
@@ -53,7 +65,6 @@ class App extends React.Component<AppProps, AppState> {
         console.log("Geolocation not supported by this browser");
       }
     }
-
     const getPosition = (position: Position) => {
       this.setState({
         latitude: position.coords.latitude,
@@ -62,36 +73,30 @@ class App extends React.Component<AppProps, AppState> {
       console.log(this.state);
     };
     getLocation();
-
   }
- 
-  
-
   render() {
+    const { classes } = this.props;
     return (
-      <div style={{padding: "5px"}}>
-    
-    <AppBar position="static">
-      <Toolbar>  
-      <Typography variant="h3">
-        FOOD SPACE WEATHER
-     </Typography>
-      </Toolbar> 
-    </AppBar>
-
-        <Container>
-          <Restaurants location={this.state}  />    
-        </Container>
-        <Container>
-          <NASA/>
-        </Container>
-        <Container>
-          <OpenWeather location={this.state} />
-        </Container>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div style={{ padding: "5px" }}>
+          <AppBar position="static" color="secondary">
+            <Toolbar>
+              <Typography variant="h3">FOOD SPACE WEATHER</Typography>
+            </Toolbar>
+          </AppBar>
+​
+          <Container>
+            <Restaurants location={this.state} />
+          </Container>
+          <Container>
+            <NASA />
+          </Container>
+          <Container>
+            <OpenWeather location={this.state} />
+          </Container>
+        </div>
+      </MuiThemeProvider>
     );
   }
-
 }
-
 export default withStyles(styles, { withTheme: true })(App);
